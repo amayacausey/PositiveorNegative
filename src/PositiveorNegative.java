3bcad90bec9c5be3c88af3d02db4cbd120c7df2f
amayacausey <amayacausey@gmail.com>
2020-06-16 13:33:02 -0700

    first commit

---
+++ src/PositiveorNegative.java
@@ -0,0 +1,15 @@
+public class PositiveorNegative {
+    public static void main(String[] args){
+
+        int num= 35;
+
+        if (num>=0) {
+            System.out.println( num + " is positive");
+        }
+        else {
+            System.out.println( num +" is negative");
+
+
+        }
+    }
+}
